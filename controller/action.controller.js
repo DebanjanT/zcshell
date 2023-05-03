32f618704f8df9750fb788ba636411b92a4aeec8
Debanjan Tewary <89004599+DebanjanT@users.noreply.github.com>
2023-05-04 01:10:40 +0530

    fix: ansi fix

--- controller/action.controller.js
+++ controller/action.controller.js
@@ -4,9 +4,13 @@ const exec = util.promisify(require("child_process").exec);
 export const startServer = async (req, res) => {
   try {
     const { stdout, stderr } = await exec("bash scripts/start_server.sh");
+    const result = stdout.replace(
+      /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+      ""
+    );
     return res.status(200).json({
       status: "success",
-      message: stdout,
+      message: result,
     });
   } catch (err) {
     return res.status(400).json({
@@ -14,12 +18,17 @@ export const startServer = async (req, res) => {
     });
   }
 };
+
 export const stopServer = async (req, res) => {
   try {
     const { stdout, stderr } = await exec("bash scripts/stop_server.sh");
+    const result = stdout.replace(
+      /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+      ""
+    );
     return res.status(200).json({
       status: "success",
-      message: stdout,
+      message: result,
     });
   } catch (err) {
     return res.status(400).json({
@@ -27,26 +36,40 @@ export const stopServer = async (req, res) => {
     });
   }
 };
+
 export const serverDetails = async (req, res) => {
   try {
     const { stdout, stderr } = await exec("bash scripts/server_details.sh");
-    return res.status(200).json({
-      status: "success",
-      message: stdout,
-    });
+    const result = stdout.replace(
+      /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+      ""
+    );
+    if (!stderr) {
+      return res.status(200).json({
+        status: "success",
+        message: result,
+      });
+    }
   } catch (err) {
     return res.status(400).json({
       error: err,
     });
   }
 };
+
 export const serverMonitor = async (req, res) => {
   try {
     const { stdout, stderr } = await exec("bash scripts/server_monitor.sh");
-    return res.status(200).json({
-      status: "success",
-      message: stdout,
-    });
+    const result = stdout.replace(
+      /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g,
+      ""
+    );
+    if (!stderr) {
+      return res.status(200).json({
+        status: "success",
+        message: result,
+      });
+    }
   } catch (err) {
     return res.status(400).json({
       error: err,
