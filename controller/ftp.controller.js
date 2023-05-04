f6b65a40a9ed8890e65abe788c221f5b0a0966a1
Debanjan Tewary <89004599+DebanjanT@users.noreply.github.com>
2023-05-04 22:18:32 +0530

    Change pathing

--- controller/ftp.controller.js
+++ controller/ftp.controller.js
@@ -45,8 +45,8 @@ export const fetchFiles = async (req, res) => {
 export const readFile = async (req, res) => {
   try {
     const { FILE_PATH } = req.body;
-    const FILE_DIR = path.join(USER_SERVER_DIR, FILE_PATH);
-    const data = fs.readFileSync(FILE_DIR, { encoding: "utf8", flag: "r" });
+    // const FILE_DIR = path.join(USER_SERVER_DIR, FILE_PATH);
+    const data = fs.readFileSync(FILE_PATH, { encoding: "utf8", flag: "r" });
     return res.status(200).json({
       status: "success",
       message: "File fetched successfully",
@@ -63,14 +63,15 @@ export const readFile = async (req, res) => {
 
 export const readFolder = async (req, res) => {
   try {
-    const HOME_DIR = process.env.HOME;
-    const FOLDER_PATH = req.query.fp;
-    const FOLDER_DIR = path.join(HOME_DIR + FOLDER_PATH);
+    // const HOME_DIR = process.env.HOME;
+    // const FOLDER_PATH = req.query.fp;
+    // const FOLDER_DIR = path.join(HOME_DIR + FOLDER_PATH);
+    const { FOLDER_PATH } = req.body;
 
     let fileList = new Array();
 
     const content = fs
-      .readdirSync(FOLDER_DIR, {
+      .readdirSync(FOLDER_PATH, {
         withFileTypes: true,
         recursive: true,
       })
@@ -79,13 +80,13 @@ export const readFolder = async (req, res) => {
           fileList.push({
             name: file.name,
             type: "folder",
-            dir: path.join(FOLDER_DIR, file.name),
+            dir: path.join(FOLDER_PATH, file.name),
           });
         } else {
           fileList.push({
             name: file.name,
             type: "file",
-            dir: path.join(FOLDER_DIR, file.name),
+            dir: path.join(FOLDER_PATH, file.name),
           });
         }
       });
