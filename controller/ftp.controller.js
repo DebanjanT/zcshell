45381fd0cfe4d2301d98e4420a176cdfd7da8833
Debanjan Tewary <89004599+DebanjanT@users.noreply.github.com>
2023-05-04 18:54:04 +0530

    ftp read custom folder

--- controller/ftp.controller.js
+++ controller/ftp.controller.js
@@ -38,3 +38,61 @@ export const fetchFiles = async (req, res) => {
     });
   }
 };
+
+export const readFile = async (req, res) => {
+  try {
+    const { FILE_PATH } = req.body;
+    const FILE_DIR = path.join(USER_SERVER_DIR, FILE_PATH);
+    const data = fs.readFileSync(FILE_DIR, { encoding: "utf8", flag: "r" });
+    return res.status(200).json({
+      status: "success",
+      message: "File fetched successfully",
+      config: data,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      status: "error",
+      message: "Could not read file",
+      error: err,
+    });
+  }
+};
+
+export const readFolder = async (req, res) => {
+  try {
+    const HOME_DIR = process.env.HOME;
+    const FOLDER_PATH = req.query.fp;
+    const FOLDER_DIR = path.join(HOME_DIR + FOLDER_PATH);
+
+    let fileList = new Array();
+
+    const content = fs
+      .readdirSync(FOLDER_DIR, {
+        withFileTypes: true,
+        recursive: true,
+      })
+      .forEach((file) => {
+        if (file.isDirectory()) {
+          fileList.push({
+            name: file.name,
+            type: "folder",
+          });
+        } else {
+          fileList.push({
+            name: file.name,
+            type: "file",
+          });
+        }
+      });
+
+    return res.status(200).json({
+      fileList: fileList,
+    });
+  } catch (err) {
+    return res.status(400).json({
+      status: "error",
+      message: "Could not read file",
+      error: err,
+    });
+  }
+};
--- routes/ftp.routes.js
+++ routes/ftp.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { fetchFiles } from "../controller/ftp.controller";
+import { fetchFiles, readFile, readFolder } from "../controller/ftp.controller";
 
 const router = express.Router();
 
 router.get("/ftp/fetchfiles", fetchFiles);
+router.post("/ftp/readfile", readFile);
+router.get("/ftp/readfolder", readFolder);
 
 module.exports = router;
